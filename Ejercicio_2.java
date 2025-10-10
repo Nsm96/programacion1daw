ca9492fe85fc8154a487014ff9eb583a0e3a1341
NicoSerrano <nicolasserranomorante@gmail.com>
2025-10-10 20:03:20 +0200

    Add files via upload
    ejercicios actividad 3

---
+++ Ejercicio_2.java
@@ -0,0 +1,40 @@
+//import java.util.Scanner;
+//
+//public class Ejercicio_2 {
+//    static void main() {
+//        Scanner sc = new Scanner(System.in);
+//        double descuento_regular = 5;
+//        double descuento_premium = 10;
+//        double descuento_vip = 20;
+//        double precio_producto;
+//        String tipo_cliente = "";
+//        double precio_con_descuento;
+//        double precio_con_descuento2;
+//        double precio_con_descuento3;
+//        double precio_total_regular;
+//        double precio_total_premium;
+//        double precio_total_vip;
+//
+//
+//        System.out.println("Indica el tipo de cliente que eres (Regular, Premium o VIP): ");
+//        tipo_cliente = sc.nextLine();
+//        System.out.println("Introduce el precio del producto: ");
+//        precio_producto = sc.nextDouble();
+//
+//        if (tipo_cliente.equals("regular")) {
+//            precio_con_descuento = precio_producto * (descuento_regular / 100);
+//            precio_total_regular = precio_producto - precio_con_descuento;
+//            System.out.println("El precio del producto con el descuento de cliente regular es: " + precio_total_regular + " €.");
+//
+//        } else if (tipo_cliente.equals("premium")){
+//            precio_con_descuento2 = precio_producto* (descuento_premium / 100);
+//            precio_total_premium = precio_producto - precio_con_descuento2;
+//            System.out.println("El precio del producto con el descuento de cliente premium es: " + precio_total_premium + " €.");
+//        } else if (tipo_cliente.equals("vip")){
+//            precio_con_descuento3 = precio_producto * (descuento_vip / 100);
+//            precio_total_vip = precio_producto - precio_con_descuento3;
+//            System.out.println("El precio del producto con el descuento de cliente VIP es: " + precio_con_descuento3 + " €.");
+//        } else
+//        System.out.println("No ha indicado ningún tipo de cliente válido.");
+//    }
+//}
---
+++ Ejercicio_4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio_4 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int dia;
+        int mes;
+        int anio;
+
+        System.out.println("Introduce el día: ");
+        dia = sc.nextInt();
+        System.out.println("Introduce el número del mes: ");
+        mes = sc.nextInt();
+        System.out.println("Introduce el año: ");
+        anio = sc.nextInt();
+    }
+}

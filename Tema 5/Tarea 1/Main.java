0253aae919e0c1c265bc1e62dc8f77857b679cb4
NicoSerrano <nicolasserranomorante@gmail.com>
2025-11-26 03:15:26 +0100

    PDO: Tarea 1
    Ejercicio tarea 1 PDO

---
+++ Tema 5/Tarea 1/Main.java
@@ -0,0 +1,32 @@
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String bf_si_no;
+
+        //  creo mis productos
+
+        Tienda impresora_hp = new Tienda("Impresora HP", 100, 21, 5);
+        Tienda raton = new Tienda("raton", 20, 4, 15);
+        Tienda libro_java = new Tienda("libro java", 20, 4, 15);
+
+        // imprimo los productos
+        System.out.println(impresora_hp);
+        System.out.println(raton);
+        System.out.println(libro_java);
+        System.out.println("Número de prodcutos: " + Tienda.getNum_productos());
+
+        System.out.println("¿Es BlackFriday?: ");
+        bf_si_no = sc.next().toLowerCase();
+
+        // ¿BF? calcular el descuento con blackfriday
+        if (bf_si_no.equals("si")){
+            Tienda.setDescuento_global(10);
+            System.out.println("Descuento global aplicado: 10%");
+        }
+        // En caso de ser BF, imprimo el precio con descuento
+        System.out.println("\n --- Con descuento de Black Friday---");
+        System.out.println(impresora_hp);
+        System.out.println(raton);
+        System.out.println(libro_java);
+        System.out.println("Número de productos: " + Tienda.getNum_productos());
+
+    }
---
+++ Tema 5/Tarea 1/Tienda.java
@@ -0,0 +1,94 @@
+public class Tienda {
+    protected String producto;
+    protected double precio;
+    protected int iva;
+    protected int descuento;
+
+    private static int num_productos = 0;
+    private static int descuento_global = 0;
+
+    // constructor
+    public Tienda() {
+        this.precio = 10;
+        this.iva = 21;
+        this.descuento = 0;
+        num_productos++;
+        this.calcularPrecio();
+    }
+
+    public Tienda(String producto, double precio, int iva, int descuento) {
+        this.producto = producto;
+        this.precio = precio;
+        this.iva = iva;
+        this.descuento = descuento;
+        num_productos++;
+    }
+
+
+    public double calcularPrecio() {
+        int descuento_Aplicado = Math.max(descuento, descuento_global);
+        double precioConDescuento = precio - (precio * descuento_Aplicado / 100.0);
+        return precioConDescuento + (precioConDescuento * iva / 100.0);
+
+    }
+
+    // to string
+
+    @Override
+    public String toString() {
+        return "Tienda{" +
+                "producto='" + producto + '\'' +
+                ", precio=" + precio +
+                ", iva=" + iva +
+                ", descuento=" + descuento +
+                ", precio total= " + calcularPrecio() +
+                '}';
+    }
+
+
+    // get y set
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getIva() {
+        return iva;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
+    public int getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(int descuento) {
+        this.descuento = descuento;
+    }
+
+    public static int getNum_productos() {
+        return num_productos;
+    }
+
+    public static int getDescuento_global() {
+        return descuento_global;
+    }
+
+    public static void setDescuento_global(int descuento_global) {
+        Tienda.descuento_global = descuento_global;
+    }
+}

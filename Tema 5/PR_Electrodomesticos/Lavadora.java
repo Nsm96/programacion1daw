fa445db1324e5cfed10773b90b4ba8e6f1b632d8
NicoSerrano <nicolasserranomorante@gmail.com>
2025-11-28 04:08:55 +0100

    Ejercicio terminado

--- Tema 5/PR_Electrodomesticos/Lavadora.java
+++ Tema 5/PR_Electrodomesticos/Lavadora.java
@@ -1,24 +1,53 @@
 public class Lavadora extends Electrodomestico {
-    protected int carga = 5;
+    protected int carga;
+    double precio;
 
-    
-    public Lavadora() {
+    public Lavadora(int precio_base, Colores color, String consumo_energetico, int peso, int carga) {
+        super(precio_base, color, consumo_energetico, peso);
+        this.carga = carga;
+        PrecioFinal();
     }
 
-    public Lavadora(int precio_base, Colores color, String consumo_energetico, int peso) {
-        super(precio_base, color, consumo_energetico, peso);
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
     }
 
     @Override
     public String toString() {
-        return "Lavadora{" +
-                "peso=" + peso +
-                ", consumo_energetico='" + consumo_energetico + '\'' +
-                ", color=" + color +
-                ", precio_base=" + precio_base +
-                '}';
+        return "Lavadora" +
+                "\n * Carga= " + carga +
+                "\n * Consumo Energético = " + consumo_energetico +
+                "\n * Peso = " + peso +
+                "\n * Precio = " + precio + " €";
     }
 
+    @Override
+    protected int PrecioFinal() {
+        precio = super.PrecioFinal();
+        if (carga > 6) {
+            precio += 50;
+        }
+        return (int) precio;
+    }
 
 }
+
+
+
+
+
+
+
 

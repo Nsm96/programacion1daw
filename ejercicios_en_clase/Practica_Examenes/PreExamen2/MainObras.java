b107dd21628b55fcfe0b44031f8cba0b00a06595
NicoSerrano <nicolasserranomorante@gmail.com>
2025-12-02 05:18:32 +0100

    V2 del main

--- ejercicios_en_clase/Practica_Examenes/PreExamen2/MainObras.java
+++ ejercicios_en_clase/Practica_Examenes/PreExamen2/MainObras.java
@@ -1,4 +1,18 @@
-public class MainObras {
+static void main() {
+    Libro milibro = Libro.crearLibroPorConsola();
+    System.out.println(milibro);
+
+    Pelicula mipelicula = Pelicula.crearPeliculaPorConsola();
+    System.out.println(mipelicula);
+
+    Serie miSerie1 = new Serie("Los de daw", "Antonio", 2025, 2, true);
+    System.out.println(miSerie1);
+    Serie miSerie2 = new Serie("Los de daw Return","Antonio", 2026,2,false);
+    System.out.println(miSerie2);
+
+    milibro.compararAnio(mipelicula);
 
 }
+
+
 
